28facb9fb7ac40307cb2df1c10271a48ad9f6fd8
Ryan Faerman <ryan@faerman.net>
2014-09-08 23:19:56 -0400

    remove erroneous binding.pry

--- lib/trepscore/services/service.rb
+++ lib/trepscore/services/service.rb
@@ -245,7 +245,6 @@ module TrepScore
           file.downcase!
           file.sub! /.*:/, ''
           doc_file = File.expand_path("../../../../doc/#{file}", __FILE__)
-          binding.pry
           File.exists?(doc_file) ? File.read(doc_file) : ""
         end
 
