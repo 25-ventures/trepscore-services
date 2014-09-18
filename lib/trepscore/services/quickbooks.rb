5f8e4db45f1ce46f9703fc28ce4a500f242ebdf4
Ryan Faerman <ryan@faerman.net>
2014-09-18 18:17:06 -0400

    fix style issues

--- lib/trepscore/services/quickbooks.rb
+++ lib/trepscore/services/quickbooks.rb
@@ -8,7 +8,6 @@ require 'quickbooks-ruby'
 module Quickbooks
   module Service
     class Reports < BaseService
-
       def profit_and_loss(params = {})
         url = url_for_resource('reports/ProfitAndLoss')
         url = add_query_string_to_url(url, params)
@@ -16,29 +15,27 @@ module Quickbooks
         xml = @last_response_xml
 
         {
-                  reportName: xml.css('ReportName').text,
-                 reportBasis: xml.css('ReportBasis').text,
-                 startPeriod: xml.css('StartPeriod').text.to_date,
-                   endPeriod: xml.css('EndPeriod').text.to_date,
-                 totalIncome: value_of('Income'),
-                 grossProfit: value_of('GrossProfit'),
-                    expenses: value_of('Expenses'),
+          reportName: xml.css('ReportName').text,
+          reportBasis: xml.css('ReportBasis').text,
+          startPeriod: xml.css('StartPeriod').text.to_date,
+          endPeriod: xml.css('EndPeriod').text.to_date,
+          totalIncome: value_of('Income'),
+          grossProfit: value_of('GrossProfit'),
+          expenses: value_of('Expenses'),
           netOperatingIncome: value_of('NetOperatingIncome'),
-               otherExpenses: value_of('OtherExpenses'),
-              netOtherIncome: value_of('NetOtherIncome'),
-                   netIncome: value_of('NetIncome'),
+          otherExpenses: value_of('OtherExpenses'),
+          netOtherIncome: value_of('NetOtherIncome'),
+          netIncome: value_of('NetIncome'),
         }
       end
 
       def value_of(group)
         nodes = @last_response_xml.css("Row[group=#{group}] Summary ColData:last")
-        value = if nodes.nil? || nodes.empty?
+        if nodes.nil? || nodes.empty?
           0.0
         else
           nodes.first['value'].to_f
         end
-
-        value
       end
     end
   end
@@ -48,11 +45,8 @@ module TrepScore
   module Services
     class Quickbooks < Service
       title 'QuickBooks Online'
-
       category :bookkeeping
-
-      url   'https://qbo.intuit.com'
-
+      url 'https://qbo.intuit.com'
       logo_url 'https://images.appcenter.intuit.com/Content/Static/4.12.0-trunk-2052/images/quickbooks_syncBig.png'
 
       maintained_by github: 'ryanfaerman',
@@ -62,8 +56,8 @@ module TrepScore
       oauth(provider: :quickbooks) do |response, params|
         {
           realm_id: params['realmId'],
-          token: response['credentials']['token'],
-          secret: response['credentials']['secret'],
+          token:    response['credentials']['token'],
+          secret:   response['credentials']['secret'],
         }
       end
 
@@ -71,7 +65,7 @@ module TrepScore
         start_date = period.first.strftime('%Y-%m-%d')
         end_date   = period.last.strftime('%Y-%m-%d')
 
-        report = client.profit_and_loss({start_date: start_date, end_date: end_date})
+        report = client.profit_and_loss(start_date: start_date, end_date: end_date)
 
         {
           income: report[:totalIncome],
@@ -79,34 +73,33 @@ module TrepScore
         }
       end
 
-      def test
-
-      end
-
       def client
         @client ||= begin
-          ::Quickbooks::Service::Reports.new({
-            company_id: data['realm_id'],
+          ::Quickbooks::Service::Reports.new(
+            company_id:   data['realm_id'],
             access_token: oauth_access_token
-          })
+          )
         end
       end
 
       def oauth_consumer_token
-          @oauth_consumer_token ||= OAuth::Consumer.new(ENV['QUICKBOOKS_KEY'], ENV['QUICKBOOKS_SECRET'], {
-              :site                 => "https://oauth.intuit.com",
-              :request_token_path   => "/oauth/v1/get_request_token",
-              :authorize_url        => "https://appcenter.intuit.com/Connect/Begin",
-              :access_token_path    => "/oauth/v1/get_access_token"
-          })
-        end
+        @oauth_consumer_token ||= OAuth::Consumer.new(
+          ENV['QUICKBOOKS_KEY'],
+          ENV['QUICKBOOKS_SECRET'],
+          {
+            site:               'https://oauth.intuit.com',
+            request_token_path: '/oauth/v1/get_request_token',
+            authorize_url:      'https://appcenter.intuit.com/Connect/Begin',
+            access_token_path:  '/oauth/v1/get_access_token'
+          }
+        )
+      end
 
-        def oauth_access_token
-          @oauth_access_token ||= begin
-            OAuth::AccessToken.new(oauth_consumer_token, data['token'], data['secret'])
-          end
+      def oauth_access_token
+        @oauth_access_token ||= begin
+          OAuth::AccessToken.new(oauth_consumer_token, data['token'], data['secret'])
         end
-
+      end
     end
   end
 end
