f62bcbf9c9259c0bb0a0f95e026d84a72633aa88
Ryan Leavengood <leavengood@gmail.com>
2014-09-19 18:11:37 -0400

    Add back paging and fix a bunch of other things
    This ensures that stats are correct even when going fairly far in the
    past.
    
    Apparently Octokit.auto_paginate does not work as described, or at all
    really.
    
    Also open issues and closed issues are handled correctly such that if an
    issue is opened and closed in the same week the numbers should be
    open_issues: 1 and closed_issues: 1.

--- spec/lib/trepscore/services/github/client_spec.rb
+++ spec/lib/trepscore/services/github/client_spec.rb
@@ -2,7 +2,7 @@ require 'trepscore/services/github'
 
 describe TrepScore::Services::GitHub::Client do
   let(:octokit) do
-    double('Octokit client', commits: [], list_issues: [], same_options?: false)
+    double('Octokit client', commits: [], issues: [], same_options?: false)
   end
 
   let(:end_date) { Date.today }
@@ -24,38 +24,58 @@ describe TrepScore::Services::GitHub::Client do
   let(:metrics) { described_class.new(data).metrics }
 
   it 'returns the total number of commits' do
-    allow(octokit).to receive(:commits) do
-      [
-        { commit: { author: { date: (Date.today - 5).to_time } } },
-        # Be sure to test we can find things within the end date
-        { commit: { author: { date: (Date.today).to_time + 1000 } } },
-        { commit: { author: { date: (Date.today + 1).to_time } } },
-      ]
+    allow(octokit).to receive(:commits) do |repo, _, since:, until:, page: 1|
+      case page
+      when 1
+        [
+          { sha: 1, commit: { author: { date: (Date.today - 5).to_time } } },
+          # Be sure to test we can find things within the end date
+          { sha: 2, commit: { author: { date: (Date.today).to_time + 1000 } } },
+          { sha: 3, commit: { author: { date: (Date.today + 1).to_time } } },
+        ]
+      when 2
+        [
+          # Repeat this on purpose
+          { sha: 3, commit: { author: { date: (Date.today + 1).to_time } } },
+        ]
+      else
+        []
+      end
     end
 
-    expect(metrics[:total_commits]).to equal(2)
+    expect(metrics[:total_commits]).to eq(2)
   end
 
   context 'issues' do
     before do
-      allow(octokit).to receive(:list_issues) do
-        [
-          { created_at: (Date.today - 5).to_time, state: 'open' },
-          { created_at: (Date.today - 4).to_time, state: 'closed' },
-          { created_at: (Date.today - 3).to_time, state: 'open', pull_request: {} },
-          # Be sure to test we can find things within the end date
-          { created_at: (Date.today).to_time + 1000, state: 'open' },
-          { created_at: (Date.today + 1).to_time },
-        ]
+      allow(octokit).to receive(:issues) do |repo, state:, since:, page: 1, direction:|
+        case page
+        when 1
+          [
+            { number: 1, created_at: (Date.today - 10).to_time, closed_at: (Date.today - 5).to_time },
+            { number: 2, created_at: (Date.today - 5).to_time },
+            { number: 3, created_at: (Date.today - 4).to_time, closed_at: (Date.today - 2).to_time},
+          ]
+        when 2
+          [
+            { number: 4, created_at: (Date.today - 3).to_time, pull_request: {} },
+            # Be sure to test we can find things within the end date
+            { number: 5, created_at: (Date.today).to_time + 1000 },
+            { number: 6, created_at: (Date.today - 1).to_time, closed_at: (Date.today + 1).to_time },
+            { number: 7, created_at: (Date.today + 1).to_time },
+          ]
+        else
+          []
+        end
       end
     end
 
     it 'returns the number of issues open in the period' do
-      expect(metrics[:open_issues]).to eq 2
+      expect(metrics[:open_issues]).to eq 4
     end
 
     it 'returns the number of issues closed in the period' do
-      expect(metrics[:closed_issues]).to eq 1
+      expect(metrics[:closed_issues]).to eq 2
     end
 
     it 'returns the number of pull requests in the period' do
